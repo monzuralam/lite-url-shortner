5a2d40751429f288f5fa2036fd3c2979d487c52c
monzuralam <monzur707@gmail.com>
2023-10-12 16:28:27 +0600

    mobile responsive issue has been fixed on header

--- App/views/welcome.view.php
+++ App/views/welcome.view.php
@@ -5,9 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Lite URL Shortner</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="public/assets/css/fonts.css">
+    <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 
 <body>
+    <header class="relative py-5">
+        <div class="container mx-auto">
+            <div class="flex items-center justify-between gap-4 md:flex-row flex-col">
+                <div>
+                    <a href="/">
+                        <img src="public/assets/images/logo.png" alt="LiteURLShortner" class="w-full h-12">
+                    </a>
+                </div>
+                <nav class="flex justify-between">
+                    <ul class="flex">
+                        <li><a href="/why-lite-url-shortner" class="px-4">Why?</a></li>
+                        <li><a href="/products" class="px-4">Products</a></li>
+                        <li><a href="/pricing" class="px-4">Pricing</a></li>
+                        <li><a href="/resources" class="px-4">Resources</a></li>
+                        <li><a href="/quote" class="px-4">Quote</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
     <h1>Lite URL Shortner</h1>
 </body>
 
