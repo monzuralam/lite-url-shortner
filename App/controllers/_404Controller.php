f5701da14bb280e71a2a993aa26717c51d8b111d
monzuralam <monzur707@gmail.com>
2023-10-12 17:21:57 +0600

    _404Controller, view & style fix for 404

---
+++ App/controllers/_404Controller.php
@@ -0,0 +1,6 @@
+<?php
+class _404Controller extends Controller {
+    public function index() {
+        $this->view('404');
+    }
+}
---
+++ App/views/404.view.php
@@ -0,0 +1,14 @@
+<?php
+require_once "header.php";
+?>
+<section class="error relative py-10">
+    <div class="container mx-auto">
+        <img src="public/assets/images/404.png" alt="404" class="md:w-1/5 w-full mx-auto mb-10">
+        <div class="max-w-3xl mx-auto px-5">
+            <h1 class="text-6xl font-bold mb-5">Something's wrong here.</h1>
+            <p class="text-xl font-light">This is a 404 error, which means you've clicked on a bad link or entered an invalid URL. Maybe what you are looking for can be found at <a href="/" class="underline">LiteURLShortner</a>. P.S. Bitly links are case sensitive.</p>
+        </div>
+    </div>
+</section>
+<?php
+require_once "footer.php";
